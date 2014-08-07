2ec057c6847485c8b696aa392b4e6083e17a84d2
Eduardo Piva <efpiva@gmail.com>
2014-08-07 19:03:36 -0300

    Tutorial running version

---
+++ DoverTutorial/Form1.b1f.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using SAPbouiCOM.Framework;
+using SAPbouiCOM;
+using Dover.Framework.Attribute;
+using Dover.Framework.Form;
+
+namespace DoverTutorial
+{
+    [MenuEvent(UniqueUID ="doverTutForm")]
+    [FormAttribute("DoverTutorial.Form1", "Form1.b1f")]
+    public class Form1 : DoverUserFormBase
+    {
+        public Form1()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+    }
+}
