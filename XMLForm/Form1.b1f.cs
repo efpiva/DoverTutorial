21552418b9d49cfefabc692e3863b60588708f04
Eduardo Piva <eduardo.piva@gweadded.com.br>
2014-08-11 11:31:22 -0300

    XML Form

---
+++ XMLForm/Form1.b1f.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using SAPbouiCOM.Framework;
+using SAPbouiCOM;
+using Dover.Framework.Attribute;
+using Dover.Framework.Form;
+
+namespace DoverTutorial
+{
+    [MenuEvent(UniqueUID ="doverTutForm")]
+    [FormAttribute("QuickStart.Form1", "Form1.b1f")]
+    public class Form1 : DoverUserFormBase
+    {
+        public Form1()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("1").Specific));
+            this.Button1 = ((SAPbouiCOM.Button)(this.GetItem("2").Specific));
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private Button Button0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private Button Button1;
+    }
+}
---
+++ XMLForm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Dover.Framework;
+using Dover.Framework.Attribute;
+using SAPbouiCOM;
+
+namespace DoverTutorial
+{
+    [Menu(String="DoverTutorial", Type=BoMenuType.mt_POPUP, UniqueID="doverTut", FatherUID="43520")]
+    [Menu(String="My Form", Type=BoMenuType.mt_STRING, UniqueID="doverTutForm", FatherUID="doverTut")]
+    [AddIn(Description="My Test App", B1SResource="QuickStart.QuickStart.b1s")]
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Dover.Framework.Application app = new Dover.Framework.Application();
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
