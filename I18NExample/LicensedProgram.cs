34f54a4d35a49609382b1548314b312b46a902d9
Eduardo Piva <efpiva@gmail.com>
2015-04-20 20:05:26 -0300

    Multiple projects with same source, one with license control and another without

---
+++ I18NExample/LicensedProgram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Dover.Framework;
+using Dover.Framework.Attribute;
+using SAPbouiCOM;
+
+namespace I18NExample
+{
+
+    [ResourceBOM("I18NExample.UDO.xml", ResourceType.UDO)]
+    [ResourceBOM("I18NExample.UDF.xml", ResourceType.UserField)]
+    [ResourceBOM("I18NExample.UDT.xml", ResourceType.UserTable)]
+    [Menu(String="DoverTutorial", Type=BoMenuType.mt_POPUP, UniqueID="doverTut", FatherUID="43520")]
+    [Menu(String="My Form", Type=BoMenuType.mt_STRING, UniqueID="doverTutForm", FatherUID="doverTut")]
+    [AddIn(Name="I18NExample", Description="My Test App", Namespace="DOVER_WL", LicenseFile="I18NExample.publicKey.xml")]
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Dover.Framework.Application app = new Dover.Framework.Application();
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
